a41fdbceabaac4c225a3a0d22b9a74a8f32c7370
anto-christo <anto.christo.20@gmail.com>
2018-06-25 16:25:52 +0530

    Initiate as FrappeJS application

--- server.js
+++ server.js
@@ -1,37 +1,11 @@
-var express = require('express');
-var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io').listen(server);
+const server = require('frappejs/server');
 
-server.listen(process.env.PORT || 8080,function(){
-    console.log('Listening on '+server.address().port);
-});
-
-io.on('connection',function(socket){
-
-    socket.emit('giveID',socket.id);
-
-    socket.on('create', function (masterID) {
-        io.to(masterID).emit('created',socket.id);
-    });
-
-    socket.on('join', function (creatorID) {
-        io.to(creatorID).emit('joined',socket.id);
-    });
-
-    socket.on('ready', function (masterID){
-        io.to(masterID).emit('createOffer',socket.id);
-    });
-
-    socket.on('offer', function(event){
-        io.to(event.creatorID).emit('sendOffer',{offer:event.offer, id:socket.id});
-    });
-
-    socket.on('answer', function(event){
-        io.to(event.masterID).emit('sendAnswer',{answer:event.answer, id:socket.id});
-    });
-
-    socket.on('candidate', function (event){
-        io.to(event.socketID).emit('candidate', {event:event, id:socket.id});
-    });
+server.start({
+    backend: 'sqlite',
+    connectionParams: {
+      dbPath: 'test.db',
+    },
+    staticPath: './dist',
+    models: require('./models'),
+    authConfig: {}
 });
